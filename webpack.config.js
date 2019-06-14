5bd4088782d08eac1b50b821f3066d9de7a724d2
Delan Azabani <delan@azabani.com>
2019-06-14 22:38:59 +1000

    set up production mode and only run CompressionPlugin there

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require("path");
+const webpack = require("webpack");
 const CompressionPlugin = require("compression-webpack-plugin");
 
-module.exports = {
+const config = {
     entry: "./src/index.ts",
     output: {
         filename: "script.js",
         path: path.resolve(__dirname, "dist"),
     },
-    mode: "development",
     devServer: {
         contentBase: "./dist",
     },
@@ -28,12 +28,24 @@ module.exports = {
             },
         ],
     },
-    plugins: [
-        new CompressionPlugin({
-            filename: '[path].br[query]',
-            algorithm: 'brotliCompress',
-            test: /[.](js|bin)$/,
-            cache: true,
-        }),
-    ],
+    plugins: [],
+};
+
+module.exports = (env, argv) => {
+    switch (argv.mode) {
+        case "production":
+            config.plugins.push(new CompressionPlugin({
+                filename: '[path].br[query]',
+                algorithm: 'brotliCompress',
+                test: /[.](js|bin)$/,
+                cache: true,
+            }));
+            break;
+        default:
+            // breaks webpack-dev-server
+            // config.plugins.push(new webpack.debug.ProfilingPlugin());
+            break;
+    }
+
+    return config;
 };
