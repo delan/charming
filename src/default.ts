3f445e5f4897111b15ceea4ece20a3e3f2fa2b1c
Delan Azabani <delan@azabani.com>
2019-06-29 18:44:15 +1000

    tests for nullToDefault

--- src/default.ts
+++ src/default.ts
@@ -1,3 +1,7 @@
+export function nullToDefault(value: boolean | null, or: boolean): boolean;
+export function nullToDefault(value: number | null, or: number): number;
+export function nullToDefault(value: string | null, or: string): string;
+export function nullToDefault(value: symbol | null, or: symbol): symbol;
 export function nullToDefault<T>(value: T | null, or: T): T {
   if (value == null) {
     return or;
