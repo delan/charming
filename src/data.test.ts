37ee988a9edae6a47aa4f3eee5aea40e0b7b2821
Delan Azabani <delan@azabani.com>
2019-06-29 20:54:22 +1000

    tests for flag predicates

--- src/data.test.ts
+++ src/data.test.ts
@@ -1,9 +1,15 @@
-import { getString } from "./data";
+import {
+  getString,
+  kDefinitionExists,
+  isEmojiPresentation,
+  isSpaceSeparator,
+  isAnyMark,
+} from "./data";
 
 const getData = () => {
   const string = [..."abcde"];
   const empty = new DataView(new ArrayBuffer(0));
-  const bits = empty;
+  const bits = new DataView(Uint8Array.of(0b10101010, 0b01010101).buffer);
   const name = new DataView(Uint8Array.of(0, 1, 0, 2).buffer);
   const gc = new DataView(Uint8Array.of(0, 3, 0, 4).buffer);
   const block = new DataView(Uint8Array.of(0, 5, 255, 255).buffer);
@@ -25,3 +31,23 @@ test("getString returns null when index is out of bounds", () =>
 
 test("getString returns null when index is sentinel", () =>
   void expect(getString(getData(), "block", 1)).toBe(null));
+
+test("kDefinitionExists returns correct value", () => {
+  expect(kDefinitionExists(getData(), 0)).toBe(false);
+  expect(kDefinitionExists(getData(), 1)).toBe(true);
+});
+
+test("isEmojiPresentation returns correct value", () => {
+  expect(isEmojiPresentation(getData(), 0)).toBe(true);
+  expect(isEmojiPresentation(getData(), 1)).toBe(false);
+});
+
+test("isSpaceSeparator returns correct value", () => {
+  expect(isSpaceSeparator(getData(), 0)).toBe(false);
+  expect(isSpaceSeparator(getData(), 1)).toBe(true);
+});
+
+test("isAnyMark returns correct value", () => {
+  expect(isAnyMark(getData(), 0)).toBe(true);
+  expect(isAnyMark(getData(), 1)).toBe(false);
+});
