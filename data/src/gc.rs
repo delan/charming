f5c0890b1b27facfcf4190d72a46796f16f7add3
Delan Azabani <delan@azabani.com>
2019-07-27 15:10:40 +1000

    replace underscores with spaces in gc labels

--- data/src/gc.rs
+++ data/src/gc.rs
@@ -9,11 +9,12 @@ pub(crate) fn gc_handler(
     sink: &mut HashMap<String, String>,
     captures: Captures,
 ) -> Result<(), Error> {
-    let key = captures.name_ok("key")?.to_owned();
-    let value = captures.name_ok("value")?.to_owned();
+    let key = captures.name_ok("key")?;
+    let value = captures.name_ok("value")?;
+    let value = value.replace('_', " ");
     let value = format!("{} ({})", value, key);
 
-    sink.insert(key, value);
+    sink.insert(key.to_owned(), value);
 
     Ok(())
 }
