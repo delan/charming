d610370747dd9e1ea042b4cc435d7cd2c9ec0c31
Delan Azabani <delan@azabani.com>
2022-03-30 02:14:27 +0800

    data: warn if clobbering name aliases

--- data/src/na.rs
+++ data/src/na.rs
@@ -15,18 +15,33 @@ pub(crate) fn na_handler(
     let r#type = captures.name_ok("type")?;
 
     if r#type == "correction" {
+        if let Some(old) = sink[point].nacorr.as_deref() {
+            eprintln!("Warning: sink[{}].nacorr being overwritten from “{}” to “{}”", point, old, alias);
+        }
         sink[point].nacorr = Some(popularity.vote(alias));
     }
     if r#type == "control" {
+        if let Some(old) = sink[point].nacont.as_deref() {
+            eprintln!("Warning: sink[{}].nacont being overwritten from “{}” to “{}”", point, old, alias);
+        }
         sink[point].nacont = Some(popularity.vote(alias));
     }
     if r#type == "alternate" {
+        if let Some(old) = sink[point].naalte.as_deref() {
+            eprintln!("Warning: sink[{}].naalte being overwritten from “{}” to “{}”", point, old, alias);
+        }
         sink[point].naalte = Some(popularity.vote(alias));
     }
     if r#type == "figment" {
+        if let Some(old) = sink[point].nafigm.as_deref() {
+            eprintln!("Warning: sink[{}].nafigm being overwritten from “{}” to “{}”", point, old, alias);
+        }
         sink[point].nafigm = Some(popularity.vote(alias));
     }
     if r#type == "abbreviation" {
+        if let Some(old) = sink[point].naabbr.as_deref() {
+            eprintln!("Warning: sink[{}].naabbr being overwritten from “{}” to “{}”", point, old, alias);
+        }
         sink[point].naabbr = Some(popularity.vote(alias));
     }
 
