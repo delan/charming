286fcd9101707831ba3693b32dc7ad9dd39233dc
Delan Azabani <delan@azabani.com>
2025-03-15 19:50:57 +0800

    revert: switch from cff to cff2 fonts
    (dce9f38e49dada4e09ea64fc76cbb3cc315cf048)

--- helper/nanoemoji.sh
+++ helper/nanoemoji.sh
@@ -10,4 +10,4 @@ set -x
 # twemoji-13.1.1/assets/svg/1f496.svg
 [ -f build/Font.ttf ] \
     || find $1 -name '*.svg' -print0 \
-    | >&2 xargs -0xs 262144 -n 999999999 nanoemoji --color_format cff2_colr_0
+    | >&2 xargs -0xs 262144 -n 999999999 nanoemoji --color_format cff_colr_0
