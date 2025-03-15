e790b4044df47b1856b89c80c6af445ad9ec6fc0
Delan Azabani <delan@azabani.com>
2025-03-15 20:05:08 +0800

    nix: pin dependencies with flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Unicode character browser";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat.url = "github:edolstra/flake-compat";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = import ./shell.nix { inherit pkgs; };
+      }
+    );
+}
